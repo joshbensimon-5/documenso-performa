9bf6ad64328922b6d4a504020b79a6d1be3625a0
Josh Bensimon <jjbensimon@me.com>
2025-06-02 15:51:38 +0200

    update branding

--- apps/remix/app/utils/meta.ts
+++ apps/remix/app/utils/meta.ts
@@ -2,11 +2,11 @@ import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
 
 export const appMetaTags = (title?: string) => {
   const description =
-    'Join Diploma Santé, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.';
+    'Join Hermione, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.';
 
   return [
     {
-      title: title ? `${title} - Diploma Santé` : 'Diploma Santé',
+      title: title ? `${title} - Hermione` : 'Hermione',
     },
     {
       name: 'description',
@@ -15,11 +15,11 @@ export const appMetaTags = (title?: string) => {
     {
       name: 'keywords',
       content:
-        'Signature Diploma Santé',
+        'Signature Hermione',
     },
     {
       name: 'author',
-      content: 'Diploma Santé',
+      content: 'Hermione',
     },
     {
       name: 'robots',
@@ -27,7 +27,7 @@ export const appMetaTags = (title?: string) => {
     },
     {
       property: 'og:title',
-      content: 'Diploma Santé - Signature',
+      content: 'Hermione - Signature',
     },
     {
       property: 'og:description',
